46f4c2b7d8983c9173bf99f5534488a481ac3830
MatiasGines <41976674+MatiasGines@users.noreply.github.com>
2022-06-24 00:12:21 -0300

    puerto actualizado

--- src/index.js
+++ src/index.js
@@ -19,5 +19,5 @@ app.use(indexRoutes)
 //añadimos el css al servidor
 app.use(express.static(join(__dirname, 'public')))
 
-app.listen(3000)
-console.log('server is listening on port 3000')
+app.listen(process.env.PORT || 3000)
+console.log('server is listening on port', process.env.PORT || 3000)
